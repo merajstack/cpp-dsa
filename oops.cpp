6cf9243c212c0baeeb8216069d3397ab904136bf
merajstack <meraj.md5862@gmail.com>
2026-09-05 21:41:23 +0530

    constructor & initialzer & encapsulation

--- oops.cpp
+++ oops.cpp
@@ -2,41 +2,71 @@
 #include <string>
 using namespace std;
 
-
-class Teacher{
-    //properties // attrbutes
+// Encapsulation: Wrapping data and member functions inside a single unit called a class.
+class bankacc {
 private:
-double salary;
-public:    
-string name ;
-string dept;
-string subject;
+    string password;
+    double balance;
+    double accountId;
 
-//setter
-void sets(double s){
-    salary =s;
-}
-//getter
-double gets(){
-    return salary;
-}
-//methods/member functions
-void changeDept(string newDept){
-    dept = newDept;
-}
+public:
+    string username;
+    string usermail;
 };
 
+// A constructor automatically runs when an object is created.
+// Its main purpose is to initialize the object.
+class Employee {
+private:
+    int salary;
+    int id;
 
+public:
+    string name;
+    string role;
+    string desk;
 
-int main (){
-    Teacher t1;
-    t1.name = "Meraj";
-    t1.dept = "C++";
-    t1.subject = "Prog";
-    t1.sets(1000);
-    
-    cout<<t1.name<<endl;
-// getting private value
-    cout<<t1.gets()<<endl;
-        return 0;
+    // Non-parameterized constructor
+    Employee() {
+        cout << "Employee data fetched" << endl;
+        name = "Meraj";//initializer
+        role = "Engineer";
+    }
+
+    // Parameterized constructor
+    Employee(string n, string r, string d) {
+        name = n;
+        role = r;
+        desk = d;
+    }
+
+    void getInfo() {
+
+        cout << name << endl;
+        cout << desk << endl;
+    }
+};
+
+int main() {
+
+    bankacc u1;
+
+    u1.usermail = "meraj@gmail.com";
+    u1.username = "Meraj";
+
+    string arr1[3] = {u1.usermail, u1.username};
+
+    for (int i = 0; i < sizeof(arr1) / sizeof(arr1[0]); i++) {
+        cout << arr1[i] << endl;
+    }
+
+    Employee e1;
+
+    cout << e1.name << endl;
+
+    Employee e2("Meraj MD", "Engineer", "desk1");
+
+    e2.getInfo();
+
+    return 0;
 }
